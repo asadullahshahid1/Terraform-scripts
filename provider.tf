282882cf7fd45ac58c5f64160a10e8a0eee09bc7
asadullahshahid1 <sandhu.asadullah911@gmail.com>
2024-07-11 16:15:39 +0500

    adding provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "912670cb-ebac-4492-bb26-5255f1c10379"
-  client_id       = "df93244f-8c19-49a9-8be3-318e1fc51cce"
-  client_secret   = "OW68Q~pUrPNz-4oZHmOaNRR.bwkMFXp9_nfl8bbq"
-  tenant_id       = "cbc5e1a5-61bf-47b3-961a-7697f3be4586"
+  client_id       = var.arm_client_id
+  client_secret   = var.arm_client_secret
+  subscription_id = var.arm_subscription_id
+  tenant_id       = var.arm_tenant_id
 }
+
 
 
 
