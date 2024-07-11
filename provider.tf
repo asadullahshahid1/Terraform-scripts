0da364f80862063b08f7043da3fd3ca750151cb9
asadullahshahid1 <sandhu.asadullah911@gmail.com>
2024-07-11 17:18:57 +0500

    changes

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
 
- /* client_id       = var.arm_client_id
+  /* client_id       = var.arm_client_id
   client_secret   = var.arm_client_secret
   subscription_id = var.arm_subscription_id
   tenant_id       = var.arm_tenant_id */
@@ -12,9 +12,9 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-   /* resource_group_name  = "tfstategroup"
+    /* resource_group_name  = "tfstategroup"
     storage_account_name = "terrastorageaccountasad"
     container_name       = "statecontainer" */
-    key                  = "mytfstate.tfstate"
+    key = "mytfstate.tfstate"
   }
 }
